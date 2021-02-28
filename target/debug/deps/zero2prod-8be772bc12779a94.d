a5a533fba6332554f7990d837fed47664a9d1a56
Victor <victor.as.nascimento@gmail.com>
2021-02-28 13:18:44 -0300

    removing target specifying dependencies

--- target/debug/deps/zero2prod-8be772bc12779a94.d
+++
@@ -1,5 +0,0 @@
-/Users/victornascimento/Documents/Projects/Rust/zero2prod/Rust-server/zero2prod/target/debug/deps/zero2prod-8be772bc12779a94.rmeta: src/main.rs
-
-/Users/victornascimento/Documents/Projects/Rust/zero2prod/Rust-server/zero2prod/target/debug/deps/zero2prod-8be772bc12779a94.d: src/main.rs
-
-src/main.rs:
--- target/rls/debug/deps/zero2prod-ec88b2c5ac9d3768.d
+++
@@ -1,5 +0,0 @@
-/Users/victornascimento/Documents/Projects/Rust/zero2prod/Rust-server/target/rls/debug/deps/zero2prod-ec88b2c5ac9d3768.rmeta: src/main.rs
-
-/Users/victornascimento/Documents/Projects/Rust/zero2prod/Rust-server/target/rls/debug/deps/zero2prod-ec88b2c5ac9d3768.d: src/main.rs
-
-src/main.rs:

faf7254a43d2c387c9a54771b9a212ad8ed42a90
Victor <victor.as.nascimento@gmail.com>
2021-02-28 11:22:30 -0300

    tests now working for subscription

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use zero2prod::startup::run;
 use std::net::TcpListener;
 use zero2prod::configuration::get_configuration;
+use sqlx::PgPool;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     // Panic if we can't read configuration
     let configuration = get_configuration().expect("Failed to read configuration.");
+    let connection = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to Postgres.");
     let address = format!("127.0.0.1:{}", configuration.application_port);
 
     let listener = TcpListener::bind(address)?;
-    run(listener)?.await
+    run(listener, connection)?.await
 }

34f7fbfedd2bd411321fa0a133c0cb2bcb0069b0
Victor <victor.as.nascimento@gmail.com>
2021-02-28 20:37:20 -0300

    hello

--- src/startup.rs
+++ src/startup.rs
@@ -1,12 +1,13 @@
 use crate::routes::{health_check, subscribe};
 use actix_web::dev::Server;
+use actix_web::web::Data;
 use actix_web::{web, App, HttpServer};
 use sqlx::PgPool;
 use std::net::TcpListener;
 
 pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
     // wrap the connection in an Arc smart pointer
-    let db_pool = web::Data::new(db_pool);
+    let db_pool = Data::new(db_pool);
     let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
